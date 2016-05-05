d00b280fb119e76beaf91f8273ea7c97257514db
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-05-05 15:53:03 +0900

    tutor

--- src/app.js
+++ src/app.js
@@ -22,38 +22,49 @@ let initialState = {
 };
 
 
-function todoApp(state = initialState, action){
+
+
+function todos(state = [], action){
     switch(action.type){
-        case 'SET_VISIBILITY_FILTER':
-            return Object.assign({}, state, {
-                visibilityFilter: action.filter
-            });
         case 'ADD_TODO':
-            return Object.assign({}, state, {
-                todos: [
+            return [
                     ...state.todos,
                     {
                         text: action.text,
                         completed: false
                     }
-                ]
-            });
+                ];
         case 'COMPLETE_TODO':
-            return Object.assign({}, state, {
-                todos: [
+            return [
                     ...state.todos.slice(0, action.index),
                     Object.assign({}, state.todos[action.index], {
                         completed: true
                     }),
                     ...state.todos.slice(action.index + 1)
-                ]
-            });
+                ];
         default:
             return state;
     }
 }
 
 
+
+function visibilityFilter(state = 'SHOW_ALL', action){
+    switch(action.type){
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter
+        default:
+            return state;
+    }
+}
+
+
+function todoApp(state = {}, action){
+    return {
+        visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+        todos: todos(state.todos, action)
+    }
+}
 
 
 
