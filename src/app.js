3c00d16e527b7261e13323ffe28d3dc23497c465
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-05-05 15:49:13 +0900

    a

--- src/app.js
+++ src/app.js
@@ -1,55 +1,76 @@
-import expect from 'expect'
-import { createStore } from 'redux'
 
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM, { render } from 'react-dom'
 
-class Counter extends Component {
-    render(){
-        return (
-            <div>
-                <h1>{this.props.value}</h1>
-                <button onClick={this.props.onIncrement}>+</button>
-                <button onClick={this.props.onDecrement}>-</button>
-            </div>
-        )
-    }
-}
+import { createStore, combineReducers } from 'redux'
+import { Provider, connect } from 'react-redux'
 
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1
-        case 'DECREMENT':
-            return state - 1
+
+
+let initialState = {
+    visibilityFilter : 'SHOW_ALL',
+    todos: [
+        {
+            text: 'Consider using Redux',
+            completed: true,
+        },
+        {
+            text: 'Keep all state in a single tree',
+            completed: false,
+        },
+    ]
+};
+
+
+function todoApp(state = initialState, action){
+    switch(action.type){
+        case 'SET_VISIBILITY_FILTER':
+            return Object.assign({}, state, {
+                visibilityFilter: action.filter
+            });
+        case 'ADD_TODO':
+            return Object.assign({}, state, {
+                todos: [
+                    ...state.todos,
+                    {
+                        text: action.text,
+                        completed: false
+                    }
+                ]
+            });
+        case 'COMPLETE_TODO':
+            return Object.assign({}, state, {
+                todos: [
+                    ...state.todos.slice(0, action.index),
+                    Object.assign({}, state.todos[action.index], {
+                        completed: true
+                    }),
+                    ...state.todos.slice(action.index + 1)
+                ]
+            });
         default:
-            return state
+            return state;
     }
 }
 
 
 
 
-const store = createStore(counter)
 
-
-const render = () => {
-    ReactDOM.render(
-        <Counter
-            value={store.getState()}
-            onIncrement={() => {store.dispatch({type: 'INCREMENT'})}}
-            onDecrement={() => {store.dispatch({type: 'DECREMENT'})}}
-        />,
-        document.getElementById('root')
-    )
-}
-
-store.subscribe(render)
-
-render()
-
-
-
+// class Counter extends Component {
+//     render(){
+//         return (
+//             <div>
+//                 <h1>{this.props.value}</h1>
+//                 <button onClick={this.props.onIncrement}>+</button>
+//                 <button onClick={this.props.onDecrement}>-</button>
+//             </div>
+//         )
+//     }
+// }
+//
+//
+//
 // const counter = (state = 0, action) => {
 //     switch (action.type) {
 //         case 'INCREMENT':
@@ -60,19 +81,25 @@ render()
 //             return state
 //     }
 // }
-//
-// const Counter = ({value, onIncrement, onDecrement}) => (
-//     <div>
-//         <h1>{value}</h1>
-//         <button onClick={onIncrement}>+</button>
-//         <button onClick={onDecrement}>-</button>
-//     </div>
-// )
-//
-//
-//
+
+
+
+
 // const store = createStore(counter)
 //
+//
+// render(
+//     <Provider store={store}>
+//         <Counter
+//             value={store.getState()}
+//             onIncrement={() => {store.dispatch({type: 'INCREMENT'})}}
+//             onDecrement={() => {store.dispatch({type: 'DECREMENT'})}}
+//             >
+//         </Counter>
+//     </Provider>,
+//     document.getElementById('root')
+// )
+
 // const render = () => {
 //     ReactDOM.render(
 //         <Counter
@@ -87,35 +114,3 @@ render()
 // store.subscribe(render)
 //
 // render()
-
-// document.addEventListener('click', () => {
-//     store.dispatch({type: 'INCREMENT'})
-// })
-
-
-// console.log(store.getState())
-// store.dispatch({type: 'INCREMENT'})
-// console.log(store.getState())
-
-
-// expect(
-//   counter(0, {type: 'INCREMENT'})
-// ).toEqual(1)
-//
-// expect(
-//   counter(1, {type: 'INCREMENT'})
-// ).toEqual(2)
-//
-// expect(
-//   counter(2, {type: 'DECREMENT'})
-// ).toEqual(1)
-//
-// expect(
-//   counter(1, {type: 'UNKNOWN'})
-// ).toEqual(1)
-//
-// expect(
-//   counter(undefined, {})
-// ).toEqual(0)
-//
-// console.log('test passed!')
